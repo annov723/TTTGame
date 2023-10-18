ebc0629178e660a7454aee1daf7cc6bd053c27ae
Anna Nowak <annov723@gmail.com>
2023-10-18 15:42:54 +0200

    checking function written by me

--- TicTacToeGame/src/TTTG.java
+++ TicTacToeGame/src/TTTG.java
@@ -1,6 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.*;
@@ -34,24 +35,100 @@ public class TTTG implements ActionListener{
 		txtl.setText( "TIC TAC TOE" );
 		txtl.setOpaque( true );
 		
+		titlep.setLayout( new BorderLayout() );
+		titlep.setBounds( 0, 0, 800, 100 );
+		
+		buttonp.setLayout( new GridLayout( 3, 3 ) );
+		buttonp.setBackground( new Color( 0, 0, 0 ) );
+		for( int i = 0; i < 9; i++ ) {
+			barray[i] = new JButton();
+			buttonp.add( barray[i] );
+			barray[i].setFont( new Font( "Ink Free", Font.BOLD, 120 ) );
+			barray[i].setFocusable( false );
+			barray[i].addActionListener( this );
+		}
+		
+		
+		
 		titlep.add( txtl );
 		frame.add( titlep, BorderLayout.NORTH );
+		frame.add( buttonp );
 		
+		firstTurn();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		for( int i = 0; i < 9; i++ ) {
+			if( e.getSource() == barray[i] ) {
+				if( p1_turn ) {
+					if( barray[i].getText() == "" ) {
+						barray[i].setForeground( new Color( 255, 0, 0 ) );
+						barray[i].setText( "X" );
+						p1_turn = false;
+						txtl.setText( "O turn" );
+					}
+				}
+				else {
+					if( barray[i].getText() == "" ) {
+						barray[i].setForeground( new Color( 0, 0, 255 ) );
+						barray[i].setText( "O" );
+						p1_turn = true;
+						txtl.setText( "X turn" );
+					}
+				}
+			}
+		}
 		
 	}
 	
 	public void firstTurn() {
+		try {
+			Thread.sleep( 2000 );
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		if( random.nextInt( 2 ) == 0 ) {
+			p1_turn = true;
+			txtl.setText( "X turn" );
+		}
+		else {
+			p1_turn = false;
+			txtl.setText( "O turn" );			
+		}
+
 	}
 	
 	public void check() {
+		for( int i = 0; i < 3; i++ ) {
+			if( barray[0 + ( i * 3 )].getText() == barray[1 + ( i * 3 )].getText() && barray[1 + ( i * 3 )].getText() == barray[2 + ( i * 3 )].getText() ) { //rows
+				if( barray[0 + ( i * 3 )].getText() == "X" ) xwin( 0 + ( i * 3 ), 1 + ( i * 3 ), 2 + ( i * 3 ) );
+				else owin( 0 + ( i * 3 ), 1 + ( i * 3 ), 2 + ( i * 3 ) );
+				return;
+			}
+			
+			if( barray[0 + i].getText() == barray[3 + i].getText() && barray[3 + i].getText() == barray[6 + i].getText() ) { //columns
+				if( barray[0 + i].getText() == "X" ) xwin( 0 + i, 3 + i, 6 + i );
+				else owin( 0 + i, 3 + i, 6 + i );
+				return;
+			}
+		}
+		if( barray[0].getText() == barray[4].getText() && barray[4].getText() == barray[8].getText() ) { //diagonals
+			if( barray[0].getText() == "X" ) xwin( 0, 4, 8 );
+			else owin( 0, 4, 8 );
+			return;
+		}
+		if( barray[2].getText() == barray[4].getText() && barray[4].getText() == barray[6].getText() ) { //diagonals
+			if( barray[2].getText() == "X" ) xwin( 2, 4, 6 );
+			else owin( 2, 4, 6 );
+			return;
+		}
 		
 	}
+	
+	
 	
 	public void xwin( int a, int b, int c) {
 		
